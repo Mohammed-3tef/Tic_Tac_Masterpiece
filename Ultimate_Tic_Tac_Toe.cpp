fe253d2dd9dce1a38eb680d7b3996027159f907e
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2024-12-02 17:49:34 +0200

    Update Ultimate_Tic_Tac_Toe.cpp
    under development

--- Ultimate_Tic_Tac_Toe.cpp
+++ Ultimate_Tic_Tac_Toe.cpp
@@ -1,249 +1,208 @@
-// Author: Nagham Wael Mohamed
-// ID: 20231189
-// Section: S19
-// TA: Ahmed Ihab
-// Version: 1.0
-
-//--------------------------------------- HEADERS
-
-#include <bits/stdc++.h>
-#include "BoardGame_Classes.h"
-using namespace std;
-
-//--------------------------------------- HELPER FUNCTIONS
-
-void checkPlayerType(string &playerType, int num) {
-    cout << "\nWhat is player " << num << " type ?\n[1] Human.\n[2] Computer.\nChoice:";
-    getline(cin, playerType);
-    while (true) {
-        if (playerType != "1" && playerType != "2") {
-            cout << "Please enter a valid choice!\n\n";
-            cout << "What is player " << num << " type ?\n[1] Human.\n[2] Computer.\nChoice:";
-            getline(cin, playerType);
-            continue;
-        }
-        return;
-    }
-}
-
-//--------------------------------------- CLASSES
-
-template<typename T>
-class Ultimate_Tic_Tac_Toe_Board : public Board<T> {
-private:
-    bool over = false;
-public:
-    Ultimate_Tic_Tac_Toe_Board();
-    bool update_board(int x, int y, T symbol) override;
-    void display_board() override;
-    bool is_win() override;
-    bool is_draw() override;
-    bool game_is_over() override;
-};
-
-template<typename T>
-class Ultimate_Tic_Tac_Toe_Player : public Player<T> {
-public:
-    Ultimate_Tic_Tac_Toe_Player(string name, T symbol);
-    void getmove(int &x, int &y) override;
-};
-
-template<typename T>
-class Ultimate_Tic_Tac_Toe_Random_Player : public RandomPlayer<T> {
-public:
-    Ultimate_Tic_Tac_Toe_Random_Player(T symbol);
-    void getmove(int &x, int &y) override;
-};
-
-
-
-
-//--------------------------------------- IMPLEMENTATION
-
 #include <iostream>
-#include <iomanip>
-#include <cctype>
+#include <vector>
 using namespace std;
 
-
-
-// Constructor for X_O_Board
+// Base Board class from BoardGame_Classes.h
 template <typename T>
-Ultimate_Tic_Tac_Toe_Board<T>::Ultimate_Tic_Tac_Toe_Board() { //the constructor here is where we initialize the board
-    this->rows = this->columns = 3;    //3 rows and 3 columns to make the grid
-    this->board = new char*[this->rows];
-    for (int i = 0; i < this->rows; i++) {
-        this->board[i] = new char[this->columns];
-        for (int j = 0; j < this->columns; j++) {
-            this->board[i][j] = 0;   //initialized the board with zeros
-        }
-    }
-    this->n_moves = 0;
-}
+class Board {
+public:
+    virtual ~Board() {}
+    virtual bool update_board(int x, int y, T symbol) = 0;
+    virtual void display_board() = 0;
+    virtual bool is_win() = 0;
+    virtual bool is_draw() = 0;
+    virtual bool game_is_over() = 0;
+};
 
-
-
+// Player class
 template <typename T>
-bool Ultimate_Tic_Tac_Toe_Board<T>::update_board(int x, int y, T mark) {
-    // Only update if move is valid
-    if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0|| mark == 0)) {
-        if (mark == 0){
-            this->n_moves--;
-            this->board[x][y] = 0;
-        }
-        else {
-            this->n_moves++;
-            this->board[x][y] = toupper(mark);
-        }
+class Player {
+protected:
+    string name;
+    T symbol;
+    Board<T>* board;
+public:
+    Player(string n, T s) : name(n), symbol(s), board(nullptr) {}
+    void setBoard(Board<T>* b) { board = b; }
+    T getSymbol() { return symbol; }
+    virtual bool makeMove() = 0;
+};
 
-        return true;
-    }
-    return false;
-}
-
-
-
-template<typename T>
-void Ultimate_Tic_Tac_Toe_Board<T>::display_board() {
-    cout << "\n     1   2   3\n";
-    cout << "   -------------\n";
-
-    for (int i = 0; i < this->rows; i++) {
-        cout << ' ' << i + 1 << " |";
-        for (int j = 0; j < this->columns; j++) {
-            cout << " " << setw(1);
-
-            if (this->board[i][j] == 0) cout << " ";
-            else cout << this->board[i][j];
-
-            cout << " |";
-        }
-        cout << "\n   -------------\n";
-    }
-    cout << endl;
-}
-
-
-
-// Returns true if there is any winner
+// Human Player class
 template <typename T>
-bool Ultimate_Tic_Tac_Toe_Board<T>::is_win() {
-    // Check rows and columns
-    for (int i = 0; i < this->rows; i++) {
-        if ((this->board[i][0] == this->board[i][1] && this->board[i][1] == this->board[i][2] && this->board[i][0] != 0) ||
-            (this->board[0][i] == this->board[1][i] && this->board[1][i] == this->board[2][i] && this->board[0][i] != 0)) {
-            return true;
-        }
-    }
-
-    // Check diagonals
-    if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != 0) ||
-        (this->board[0][2] == this->board[1][1] && this->board[1][1] == this->board[2][0] && this->board[0][2] != 0)) {
-        return true;
-    }
-
-    return false;
-}
-
-
-
-// Return true if 9 moves are done and no winner
-template <typename T>
-bool Ultimate_Tic_Tac_Toe_Board<T>::is_draw() {
-    return (this->n_moves == 9 && !is_win());
-}
-
-
-
-template <typename T>
-bool Ultimate_Tic_Tac_Toe_Board<T>::game_is_over() {
-    return is_win() || is_draw();
-}
-
-
-
-//--------------------------------------
-
-// Constructor for X_O_Player
-template <typename T>
-Ultimate_Tic_Tac_Toe_Player<T>::Ultimate_Tic_Tac_Toe_Player(string name, T symbol) : Player<T>(name, symbol) {}
-
-
-
-template <typename T>
-void Ultimate_Tic_Tac_Toe_Player<T>::getmove(int& x, int& y) {
-    while (true) {
-        cout << "Enter your move in this form(row space column,e.g 1 3): ";
+class HumanPlayer : public Player<T> {
+public:
+    HumanPlayer(string n, T s) : Player<T>(n, s) {}
+    bool makeMove() override {
+        int x, y;
+        cout << this->name << " (" << this->symbol << "), enter your move (row and column): ";
         cin >> x >> y;
+        return this->board->update_board(x, y, this->symbol);
+    }
+};
 
-        // Convert to 0-based indexing
-        x--; y--;
+// Smaller Tic Tac Toe board
+class SmallBoard : public Board<char> {
+private:
+    vector<vector<char>> grid;
+    char winner;
+    int moves;
 
-        if (cin.fail() || x < 0 || x >= 3 || y < 0 || y >= 3) {
-            cin.clear(); // Clear error flags
-            cin.ignore(INT_MAX, '\n'); // Discard invalid input
-            cout << "Invalid input. Try again.\n";
-        } else {
-            break;
+public:
+    SmallBoard() : grid(3, vector<char>(3, '-')), winner('-'), moves(0) {}
+
+    bool update_board(int x, int y, char symbol) override {
+        if (x < 0 || x >= 3 || y < 0 || y >= 3 || grid[x][y] != '-') {
+            cout << "Invalid move. Try again.\n";
+            return false;
+        }
+        grid[x][y] = symbol;
+        moves++;
+        return true;
+    }
+
+    void display_board() override {
+        for (const auto& row : grid) {
+            for (char cell : row) {
+                cout << cell << " ";
+            }
+            cout << endl;
         }
     }
 
-}
+    bool is_win() override {
+        // Check rows, columns, and diagonals
+        for (int i = 0; i < 3; ++i) {
+            if (grid[i][0] != '-' && grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) return true;
+            if (grid[0][i] != '-' && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) return true;
+        }
+        if (grid[0][0] != '-' && grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) return true;
+        if (grid[0][2] != '-' && grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) return true;
+        return false;
+    }
 
+    bool is_draw() override {
+        return moves == 9 && !is_win();
+    }
 
+    bool game_is_over() override {
+        return is_win() || is_draw();
+    }
 
-// Constructor for X_O_Random_Player
-template <typename T>
-Ultimate_Tic_Tac_Toe_Random_Player<T>::Ultimate_Tic_Tac_Toe_Random_Player(T symbol) : RandomPlayer<T>(symbol) {
-    this->dimension = 3;
-    this->name = "Random Computer Player";
-    srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
-}
+    char get_winner()  {
+        return is_win() ? winner : '-';
+    }
 
+    char get_cell(int x, int y) const {
+        return grid[x][y];
+    }
+};
 
+// Ultimate Tic Tac Toe board
+class UltimateBoard : public Board<char> {
+private:
+    vector<vector<SmallBoard>> boards;
+    vector<vector<char>> metaBoard;
+    int activeX, activeY;
+    int moves;
 
-template <typename T>
-void Ultimate_Tic_Tac_Toe_Random_Player<T>::getmove(int& x, int& y) {
-    x = rand() % this->dimension;  // Random number between 0 and 2
-    y = rand() % this->dimension;
-}
+public:
+    UltimateBoard() : boards(3, vector<SmallBoard>(3)), metaBoard(3, vector<char>(3, '-')), activeX(-1), activeY(-1), moves(0) {}
 
+    bool update_board(int x, int y, char symbol) override {
+        int bigRow = x / 3, bigCol = y / 3;
+        int smallRow = x % 3, smallCol = y % 3;
 
+        if (activeX != -1 && activeY != -1 && (bigRow != activeX || bigCol != activeY)) {
+            cout << "You must play in the active board (" << activeX << ", " << activeY << ").\n";
+            return false;
+        }
 
+        if (!boards[bigRow][bigCol].update_board(smallRow, smallCol, symbol)) {
+            return false;
+        }
 
+        moves++;
 
+        // Update meta-board if small board is won or drawn
+        if (boards[bigRow][bigCol].is_win()) {
+            metaBoard[bigRow][bigCol] = symbol;
+        } else if (boards[bigRow][bigCol].is_draw()) {
+            metaBoard[bigRow][bigCol] = 'D';
+        }
 
-//--------------------------------------- MAIN FUNCTION
+        activeX = smallRow;
+        activeY = smallCol;
+
+        if (metaBoard[activeX][activeY] != '-') {
+            activeX = activeY = -1;
+        }
+
+        return true;
+    }
+
+    void display_board() override {
+        for (int bigRow = 0; bigRow < 3; ++bigRow) {
+            for (int smallRow = 0; smallRow < 3; ++smallRow) {
+                for (int bigCol = 0; bigCol < 3; ++bigCol) {
+                    for (int smallCol = 0; smallCol < 3; ++smallCol) {
+                        cout << boards[bigRow][bigCol].get_cell(smallRow, smallCol) << " ";
+                    }
+                    cout << "| ";
+                }
+                cout << endl;
+            }
+            cout << "---------------------\n";
+        }
+    }
+
+    bool is_win() override {
+        // Check meta-board for wins
+        for (int i = 0; i < 3; ++i) {
+            if (metaBoard[i][0] != '-' && metaBoard[i][0] == metaBoard[i][1] && metaBoard[i][1] == metaBoard[i][2]) return true;
+            if (metaBoard[0][i] != '-' && metaBoard[0][i] == metaBoard[1][i] && metaBoard[1][i] == metaBoard[2][i]) return true;
+        }
+        if (metaBoard[0][0] != '-' && metaBoard[0][0] == metaBoard[1][1] && metaBoard[1][1] == metaBoard[2][2]) return true;
+        if (metaBoard[0][2] != '-' && metaBoard[0][2] == metaBoard[1][1] && metaBoard[1][1] == metaBoard[2][0]) return true;
+        return false;
+    }
+
+    bool is_draw() override {
+        return moves >= 81 && !is_win();
+    }
+
+    bool game_is_over() override {
+        return is_win() || is_draw();
+    }
+};
+
+// Main function
 int main() {
-    string player1Type, player2Type, player1Name, player2Name;
-    Player<char> *players[2];
-    auto *gameBoard = new Ultimate_Tic_Tac_Toe_Board<char>();
+    Player<char>* players[2] = {
+            new HumanPlayer<char>("Player 1", 'X'),
+            new HumanPlayer<char>("Player 2", 'O')
+    };
 
-    cout << "<--------- Welcome To Misere Tic Tac Toe --------->\n";
-    checkPlayerType(player1Type, 1);                // Get info of player 1.
-    cout << "Please enter Player 1 name:";
-    getline(cin, player1Name);
+    UltimateBoard board;
 
-    if (player1Type == "1") {
-        players[0] = new Ultimate_Tic_Tac_Toe_Player<char>(player1Name, 'X');
+    players[0]->setBoard(&board);
+    players[1]->setBoard(&board);
+
+    int currentPlayer = 0;
+    while (!board.game_is_over()) {
+        board.display_board();
+        if (!players[currentPlayer]->makeMove()) continue;
+        currentPlayer = 1 - currentPlayer;
+    }
+
+    board.display_board();
+    if (board.is_win()) {
+        cout << "Player " << (currentPlayer == 0 ? 2 : 1) << " wins!\n";
     } else {
-        players[0] = new Ultimate_Tic_Tac_Toe_Random_Player<char>('X');
+        cout << "It's a draw!\n";
     }
 
-    checkPlayerType(player2Type, 2);                // Get info of player 2.
-    cout << "Please enter Player 2 name:";
-    getline(cin, player2Name);
-
-    if (player2Type == "1") {
-        players[1] = new Ultimate_Tic_Tac_Toe_Player<char>(player2Name,'O');
-    }
-    else {
-        players[1] = new Ultimate_Tic_Tac_Toe_Random_Player<char>('O');
-    }
-
-    // Use the customized MisereGameManager
-    GameManager<char> Ultimate_Tic_Tac_Toe_Game(gameBoard, players);
-    Ultimate_Tic_Tac_Toe_Game.run();
-
+    delete players[0];
+    delete players[1];
     return 0;
 }
