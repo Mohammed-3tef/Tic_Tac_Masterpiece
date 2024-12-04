e1c25009553e8419cb4985bf3f21a8a5fab57717
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-12-04 04:43:16 +0200

    Update Misere_Tic_Tac_Toe.cpp

--- Misere_Tic_Tac_Toe.cpp
+++ Misere_Tic_Tac_Toe.cpp
@@ -5,13 +5,13 @@
 // Version: 1.0
 
 /*
- * This is a simple implementation of the Four In A Row game.
+ * This is a simple implementation of the Misere Tic Tac Toe.
  * The game is played on a 3x3 board where each cell is filled with either 'X' or 'O'.
  * The game is played between two players:
-     ** The first player has the symbol 'X'.
-     ** The second player has the symbol 'O'.
+    ** The first player has the symbol 'X'.
+    ** The second player has the symbol 'O'.
  * The players take turns to place their symbol in an empty cell.
- * The player who places four symbols in a row, column, or diagonal loses the game.
+ * The player who places three symbols in a row, column, or diagonal loses the game.
  * The game ends in a draw if all cells are filled and no player wins.
 */
 
@@ -27,6 +27,7 @@ string name1, name2;
 int countNum = 0;
 
 //--------------------------------------- HELPER FUNCTIONS
+
 void checkPlayerType(string &playerType, int num) {
     cout << "\nWhat is player " << num << " type ?\n[1] Human.\n[2] Computer.\nChoice:";
     getline(cin, playerType);
@@ -40,13 +41,13 @@ void checkPlayerType(string &playerType, int num) {
         return;
     }
 }
+
 //--------------------------------------- CLASSES
 
-template<typename T>
-class Misere_Tic_Tac_Toe_Board : public Board<T> {
+class Misere_Tic_Tac_Toe_Board : public Board<char> {
 public:
     Misere_Tic_Tac_Toe_Board();
-    bool update_board(int x, int y, T symbol) override;
+    bool update_board(int x, int y, char symbol) override;
     void display_board() override;
     bool is_win() override;
     bool is_draw() override;
@@ -54,31 +55,29 @@ public:
     void getNames(string namePlayer1, string namePlayer2);
 };
 
-template<typename T>
-class Misere_Tic_Tac_Toe_Player : public Player<T> {
+class Misere_Tic_Tac_Toe_Player : public Player<char> {
 public:
-    Misere_Tic_Tac_Toe_Player(string name, T symbol);
+    Misere_Tic_Tac_Toe_Player(string name, char symbol);
     void getmove(int &x, int &y) override;
 };
 
-template<typename T>
-class Misere_Tic_Tac_Toe_Random_Player : public RandomPlayer<T> {
+class Misere_Tic_Tac_Toe_Random_Player : public RandomPlayer<char> {
 public:
-    Misere_Tic_Tac_Toe_Random_Player(T symbol);
+    Misere_Tic_Tac_Toe_Random_Player(char symbol);
     void getmove(int &x, int &y) override;
 };
 
 //--------------------------------------- IMPLEMENTATION
 
-template<typename T>
-void Misere_Tic_Tac_Toe_Board<T>::getNames(std::string namePlayer1, std::string namePlayer2) {
+// ---------------------------- BOARD CLASS
+
+void Misere_Tic_Tac_Toe_Board::getNames(std::string namePlayer1, std::string namePlayer2) {
     name1 = namePlayer1;
     name2 = namePlayer2;
 }
 
-template<typename T>
 // Constructor for X_O_Board
-Misere_Tic_Tac_Toe_Board<T>::Misere_Tic_Tac_Toe_Board() { //the constructor here is where we initialize the board
+Misere_Tic_Tac_Toe_Board::Misere_Tic_Tac_Toe_Board() { //the constructor here is where we initialize the board
     this->rows = this->columns = 3;    //3 rows and 3 columns to make the grid
     this->board = new char *[this->rows];
     for (int i = 0; i < this->rows; i++) {
@@ -90,8 +89,7 @@ Misere_Tic_Tac_Toe_Board<T>::Misere_Tic_Tac_Toe_Board() { //the constructor here
     this->n_moves = 0;
 }
 
-template<typename T>
-bool Misere_Tic_Tac_Toe_Board<T>::update_board(int x, int y, T mark) {
+bool Misere_Tic_Tac_Toe_Board::update_board(int x, int y, char mark) {
     // Only update if move is valid
     if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0 || mark == 0)) {
         if (mark == 0) {
@@ -107,8 +105,7 @@ bool Misere_Tic_Tac_Toe_Board<T>::update_board(int x, int y, T mark) {
     return false;
 }
 
-template<typename T>
-void Misere_Tic_Tac_Toe_Board<T>::display_board() {
+void Misere_Tic_Tac_Toe_Board::display_board() {
     cout << "\n     1   2   3\n";
     cout << "   -------------\n";
 
@@ -127,9 +124,8 @@ void Misere_Tic_Tac_Toe_Board<T>::display_board() {
     cout << endl;
 }
 
-template<typename T>
 // Returns true if there is any winner
-bool Misere_Tic_Tac_Toe_Board<T>::is_win() {
+bool Misere_Tic_Tac_Toe_Board::is_win() {
     // Check rows and columns
     for (int i = 0; i < this->rows; i++) {
         if ((this->board[i][0] == this->board[i][1] && this->board[i][1] == this->board[i][2] &&
@@ -149,22 +145,20 @@ bool Misere_Tic_Tac_Toe_Board<T>::is_win() {
 }
 
 // Return true if 9 moves are done and no winner
-template<typename T>
-bool Misere_Tic_Tac_Toe_Board<T>::is_draw() {
+bool Misere_Tic_Tac_Toe_Board::is_draw() {
     return (this->n_moves == 9 && !is_win());
 }
 
-template<typename T>
-bool Misere_Tic_Tac_Toe_Board<T>::game_is_over() {
+bool Misere_Tic_Tac_Toe_Board::game_is_over() {
     return is_win() || is_draw();
 }
 
-template<typename T>
-// Constructor for X_O_Player
-Misere_Tic_Tac_Toe_Player<T>::Misere_Tic_Tac_Toe_Player(string name, T symbol) : Player<T>(name, symbol) {}
+// ---------------------------- PLAYER CLASS
 
-template<typename T>
-void Misere_Tic_Tac_Toe_Player<T>::getmove(int &x, int &y) {
+// Constructor for X_O_Player
+Misere_Tic_Tac_Toe_Player::Misere_Tic_Tac_Toe_Player(string name, char symbol) : Player<char>(name, symbol) {}
+
+void Misere_Tic_Tac_Toe_Player::getmove(int &x, int &y) {
     if (countNum % 2 == 0) this->name = name2;
     else this->name = name1;
 
@@ -173,7 +167,8 @@ void Misere_Tic_Tac_Toe_Player<T>::getmove(int &x, int &y) {
         cin >> x >> y;
 
         // Convert to 0-based indexing
-        x--; y--;
+        x--;
+        y--;
 
         if (cin.fail() || x < 0 || x >= 3 || y < 0 || y >= 3) {
             cin.clear(); // Clear error flags
@@ -185,15 +180,15 @@ void Misere_Tic_Tac_Toe_Player<T>::getmove(int &x, int &y) {
     }
 }
 
-template<typename T>
+// ---------------------------- RANDOM PLAYER CLASS
+
 // Constructor for X_O_Random_Player
-Misere_Tic_Tac_Toe_Random_Player<T>::Misere_Tic_Tac_Toe_Random_Player(T symbol) : RandomPlayer<T>(symbol) {
+Misere_Tic_Tac_Toe_Random_Player::Misere_Tic_Tac_Toe_Random_Player(char symbol) : RandomPlayer<char>(symbol) {
     this->dimension = 3;
     srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
 }
 
-template<typename T>
-void Misere_Tic_Tac_Toe_Random_Player<T>::getmove(int &x, int &y) {
+void Misere_Tic_Tac_Toe_Random_Player::getmove(int &x, int &y) {
     if (countNum % 2 == 0) this->name = name2;
     else this->name = name1;
     x = rand() % this->dimension;  // Random number between 0 and 2
@@ -205,7 +200,7 @@ void Misere_Tic_Tac_Toe_Random_Player<T>::getmove(int &x, int &y) {
 int main(){
     string player1Type, player2Type, player1Name, player2Name;
     Player<char> *players[2];
-    auto *gameBoard = new Misere_Tic_Tac_Toe_Board<char>();
+    auto *gameBoard = new Misere_Tic_Tac_Toe_Board();
     cout << "<--------- Welcome To Misere Tic Tac Toe --------->\n";
 
     checkPlayerType(player1Type, 1);                // Get info of player 1.
@@ -221,15 +216,15 @@ int main(){
     } else player2Name = "Random Computer Player";
 
     if (player1Type == "1") {
-        players[0] = new Misere_Tic_Tac_Toe_Player<char>(player1Name, 'X');
+        players[0] = new Misere_Tic_Tac_Toe_Player(player1Name, 'X');
     } else {
-        players[0] = new Misere_Tic_Tac_Toe_Random_Player<char>('X');
+        players[0] = new Misere_Tic_Tac_Toe_Random_Player('X');
     }
 
     if (player2Type == "1") {
-        players[1] = new Misere_Tic_Tac_Toe_Player<char>(player2Name, 'O');
+        players[1] = new Misere_Tic_Tac_Toe_Player(player2Name, 'O');
     } else {
-        players[1] = new Misere_Tic_Tac_Toe_Random_Player<char>('O');
+        players[1] = new Misere_Tic_Tac_Toe_Random_Player('O');
     }
     gameBoard->getNames(player1Name, player2Name);
 
